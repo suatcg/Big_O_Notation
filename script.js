365c7caf17d6083793ed0eeefd6739b4fcb5428d
suatcg <suat-can_24@hotmail.com>
2022-08-10 00:31:09 +0300

    O(n) and O(1)

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+//#1 -- For loop in Javascript.
+const fish = ["dory", "bruce", "marlin", "nemo"];
 const nemo = ["nemo"];
+const everyone = [
+  "dory",
+  "bruce",
+  "marlin",
+  "nemo",
+  "gill",
+  "bloat",
+  "nigel",
+  "squirt",
+  "darla",
+  "hank",
+];
+
+const large = new Array(1000).fill("nemo");
 
 function findNemo(array) {
   let t0 = performance.now();
@@ -11,4 +27,29 @@ function findNemo(array) {
   console.log("call to find Nemo took " + (t1 - t0) + " milliseconds");
 }
 
-findNemo(nemo);
+// findNemo(everyone); // O(n) --> Linear Time
+
+const boxes = [0, 1, 2, 3, 4, 5];
+
+function logFirstTwoBoxes(boxes) {
+  console.log(boxes[0]); // O(1)
+  console.log(boxes[1]); // O(1)
+}
+
+logFirstTwoBoxes(boxes); // O(2) - O(1) --> Constant Time
+
+// What is the Big O of the below function ?
+function funChallenge(input) {
+  let a = 10; // O(1)
+  a = 50 + 3; // O(1)
+
+  for (let i = 0; i < input.length; i++) {
+    // O(n)
+    anotherFunction(); // O(n)
+    let stranger = true; // O(n)
+    a++; // O(n)
+  }
+  return a; // O(1)
+}
+
+// BIG O (3 + 4n) --> O(n)
